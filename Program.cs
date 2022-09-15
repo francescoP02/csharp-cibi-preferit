8939924b49cd983298ed4f4fb551b515f9111b3c
francescoP02 <file.partipilo@gmail.com>
2022-09-15 15:22:46 +0200

    Added food ranking with length, all foods, top food and less top food

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string[] favouriteFood =
+{
+    "Cannelloni ripieni", "Lasagna", "Parmigiana", "Frittata di pasta", "Pizza", "Sushi"
+};
+
+//Lunghezza della classifica
+
+Console.WriteLine(favouriteFood.Length);
+
+//Stampare la classifica con la posizione
+
+for (int i = 0; i < favouriteFood.Length; i++)
+{
+    Console.WriteLine(favouriteFood[i]);
+    Console.WriteLine(i + 1);
+};
+
+//Prima posizione
+
+Console.WriteLine(favouriteFood[0]);
+
+//Ultima posizione 
+
+Console.WriteLine(favouriteFood[favouriteFood.Length - 1]);
+
